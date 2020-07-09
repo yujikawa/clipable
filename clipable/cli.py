31b8191c883fdbd3c18d4779c482fd14a2cd18ee
yujikawa <ykyujikawa@gmail.com>
2020-07-09 13:21:21 +0900

    add linesep

--- clipable/cli.py
+++ clipable/cli.py
@@ -1,9 +1,9 @@
+import os
 import pandas as pd
 import pyperclip
 import argparse
 import io
 import pytablewriter
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,6 +11,7 @@ def main():
         add_help=True,
     )
     parser.add_argument('-f', help='Please input format type(csv)', default=None, type=str)
+    parser.add_argument('-l', help='Please input linesep', default='<br>', type=str)
 
     try:
 
@@ -21,6 +22,8 @@ def main():
             data = pd.read_csv(io.StringIO(pyperclip.paste()))
         elif args.f == 'tsv':
             data = pd.read_csv(io.StringIO(pyperclip.paste()), sep="\t")
+
+        data = data.replace(os.linesep, args.l, regex=True)
         
         data.fillna('', inplace=True)
         writer = pytablewriter.MarkdownTableWriter()
